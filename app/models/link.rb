bf2d8e4350f8adf69d50f4a652b74c172a19319e
cgibsonmm <cgibsonmm@gmail.com>
2018-08-20 00:06:30 -0400

    added class method hottest_first to links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,10 @@
 class Link < ApplicationRecord
   validates :link_url, presence: true
 
+  def self.hottest_first
+    Link.all.sort_by(&:score).reverse
+  end
+
   def upvote
     increment!(:upvotes)
   end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -17,3 +17,16 @@ RSpec.describe Link, "#score" do
     expect(link.score).to eq 1
   end
 end
+
+
+RSpec.describe Link, ".hottest_first" do
+  it "returns an array orderd form highest likes to lowest" do
+    coldest_link = create(:link, upvotes: 3, downvotes: 3)
+
+    hottest_link = create(:link, upvotes: 5, downvotes: 1)
+
+    lukewarm_link = create(:link, upvotes: 3, downvotes: 1)
+
+    expect(Link.hottest_first).to eq [hottest_link, lukewarm_link, coldest_link]
+  end
+end
